7f45cdee9f37ffd26e5a34b56dd9f8b2e9e017e7
GSDV <sdvgab@gmail.com>
2024-07-09 22:25:12 -0700

    Fixed prisma client pool issue

--- src/util/prisma/client.ts
+++ src/util/prisma/client.ts
@@ -1,13 +1,5 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production'){
-    prisma = new PrismaClient();
-}
-else {
-    if (!(global as any).prisma) (global as any).prisma = new PrismaClient();
-    prisma = (global as any).prisma;
-}
-
-export default prisma;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
