15014178501251f80063c82979793a97b668c0fd
GSDV <sdvgab@gmail.com>
2024-08-02 13:47:02 -0700

    page Create

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,42 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import CenterLayout from '@components/containers/CenterLayout';
+import NeedsToBeLoggedIn from '@components/NeedsToBeLoggedIn';
+import Create from '@components/pages/post/Create';
+
+import { Post } from '@prisma/client';
+import Loading from '@components/Loading';
+
 
 
 export default function Page() {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [freeMonths, setFreeMonths] = useState<number>(0);
+    const [pastPost, setPastPost] = useState<Post | null>(null);
+
+    const fetchFreeMonthsAndDraftedPost = async () => {
+        const res = await fetch(`/create/api/`, { method: 'GET' });
+        const resJson = await res.json();
+        if (resJson.cStatus==200) {
+            setFreeMonths(resJson.freeMonths);
+            setPastPost(resJson.draftedPost);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchFreeMonthsAndDraftedPost();
+    }, []);
+
     return (
-        <></>
-    );
+        <CenterLayout>
+            {loading ?
+                <Loading />
+            :
+                <NeedsToBeLoggedIn content={<Create freeMonths={freeMonths} pastPost={pastPost} />} />
+            }
+        </CenterLayout>
+    )
 }
