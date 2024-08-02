be190913297c01dcc35671b2c228697ae16ff8d1
GSDV <sdvgab@gmail.com>
2024-08-01 21:24:33 -0700

    AWS

---
+++ src/util/s3/aws.ts
@@ -0,0 +1,65 @@
+import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { v4 as uuidv4 } from 'uuid';
+import sharp from 'sharp';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+
+
+export const s3Client = new S3Client({
+    region: process.env.S3_REGION,
+    credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.S3_SECRETE_ACCESS_KEY as string
+    }
+});
+
+
+export const getS3AsFile = async (key: string) => {
+    const command = new GetObjectCommand({
+        Bucket: process.env.S3_REGION,
+        Key: key
+    });
+    const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 60 });
+    const response = await fetch(signedUrl);
+    const blob = await response.blob();
+    const file = new File([blob], `image ${key}`, { type: blob.type });
+    return file;
+}
+
+
+export const uploadPostPicture = async (buffer: Buffer) => {
+    const croppedBuffer = await sharp(buffer).resize({ width: 1200, height: 2100, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 1 } }).toBuffer();
+    const key = `post-f-${uuidv4()}`;
+    const res = await uploadToS3(croppedBuffer, key);
+    return key;
+}
+
+
+export const uploadPfp = async (buffer: Buffer) => {
+    const croppedBuffer = await sharp(buffer).resize({ width: 250, height: 250, fit: 'cover' }).toBuffer();
+    const key = `pfp-f-${uuidv4()}`;
+    const res = await uploadToS3(croppedBuffer, key);
+    return key;
+}
+
+
+export const uploadToS3 = async (file: Buffer, key: string) => {
+    const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: key,
+        Body: file,
+        ContentType: "image/*"
+    }
+    const cmd = new PutObjectCommand(params);
+    await s3Client.send(cmd);
+}
+
+
+export const deleteFromS3 = async (key: string) => {
+    const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: key
+    }
+    const cmd = new DeleteObjectCommand(params);
+    await s3Client.send(cmd);
+}
