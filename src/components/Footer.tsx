ebfd4d9fc1f3b79f62571c9de5e7d6299b24b637
GSDV <sdvgab@gmail.com>
2024-08-18 17:56:52 -0700

    Added target blanks to terms link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <div className={footerStyles.footer}>
             <h4 className={footerStyles.footerText}>This site is not affiliated with the University of Illinois.</h4>
-            <h5 className={footerStyles.footerText}>By using BuyIllini, you agree to the <a href='/terms-and-conditions/'>Terms and Conditions</a> and the <a href='/privacy-policy/'>Privacy Policy</a>.</h5>
+            <h5 className={footerStyles.footerText}>By using BuyIllini, you agree to the <a href='/terms-and-conditions/' target='_blank'>Terms and Conditions</a> and the <a href='/privacy-policy/' target='_blank'>Privacy Policy</a>.</h5>
         </div>
     );
 }
