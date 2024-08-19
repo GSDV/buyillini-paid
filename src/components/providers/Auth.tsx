6a1f160897f5510fa3c6eece02fe3bfca6f3c6a6
GSDV <sdvgab@gmail.com>
2024-08-18 23:43:16 -0700

    Prod debug

--- src/components/providers/Auth.tsx
+++ src/components/providers/Auth.tsx
@@ -1,3 +1,60 @@
+// 'use client';
+
+// import { createContext, useContext, useEffect, useState } from 'react';
+
+// import Cookies from 'js-cookie';
+// import { DEFAULT_PFP } from '@util/global';
+
+
+// interface AuthContextType {
+//     authToken: string,
+//     setAuthToken: React.Dispatch<React.SetStateAction<string>>,
+//     fetchCookie: () => void
+// }
+
+// const init: AuthContextType = {
+//     authToken: '',
+//     setAuthToken: ()=>{},
+//     fetchCookie: ()=>{}
+// };
+
+// const AuthContext = createContext<AuthContextType>(init);
+
+
+
+// export function Auth({ children }: { children: React.ReactNode }) {
+//     const [authToken, setAuthToken] = useState<string>('');
+//     const [pfp, setPfp] = useState<string>('');
+
+//     const fetchCookies = () => {
+//         const authCookie = Cookies.get('authtoken');
+//         const authToken = (!authCookie) ? '' : authCookie;
+//         setAuthToken(authToken);
+
+//         const pfpCookie = Cookies.get('pfp');
+//         const pfpValue = (!pfpCookie) ? '' : pfpCookie;
+//         setPfp(pfpValue);
+//     }
+
+//     useEffect(() => {
+//         console.log("USE EFFECT COOKIE");
+//         fetchCookies();
+//     }, []);
+
+//     return (
+//         <AuthContext.Provider value={{ authToken, setAuthToken, fetchCookie }} >
+//             {children}
+//         </AuthContext.Provider>
+//     );
+// }
+
+
+
+// export function useAuthContext() {
+//     return useContext(AuthContext);
+// }
+
+
 'use client';
 
 import { createContext, useContext, useEffect, useState } from 'react';
