2245f2bd4afe2d4c50f19997c2eec78ae12c6a95
GSDV <sdvgab@gmail.com>
2024-08-20 23:57:14 -0700

    Prod test

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -21,6 +21,7 @@ export default function Page() {
     const fetchFreeMonthsAndDraftedPost = async () => {
         const res = await fetch(`/create/api/`, { method: 'GET' });
         const resJson = await res.json();
+        console.log("resJson: ", resJson)
         if (resJson.cStatus==200) {
             setFreeMonths(resJson.freeMonths);
             setPastPost(resJson.draftedPost);
