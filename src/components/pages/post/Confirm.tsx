fbb4c40cd3504e56af769ec255f29243a4882e99
GSDV <sdvgab@gmail.com>
2024-08-21 12:26:48 -0700

    Prod test

--- src/components/pages/post/Confirm.tsx
+++ src/components/pages/post/Confirm.tsx
@@ -33,18 +33,20 @@ export default function ConfirmPost({ post }: { post: Post }) {
 
     return (
         <div style={{ padding: '20px', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '40px' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center', gap: '5px' }} >
-                <h1>Confirm your post</h1>
-                <h2>You are allowed to edit later</h2>
-                {alert && <Alert alert={alert} variations={[]} />}
-            </div>
-
-            <ViewPost post={post} />
-
-            <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-                <button style={{ backgroundColor: 'var(--red)' }} onClick={() => router.push('/create/')}>Back</button>
-                <button onClick={confirmPost}>Confirm</button>
-            </div>
+            <CheckIfLoading loading={loading} content={
+                <>
+                    <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center', gap: '5px' }} >
+                        <h1>Confirm your post</h1>
+                        <h2>You are allowed to edit later</h2>
+                        {alert && <Alert alert={alert} variations={[]} />}
+                    </div>
+                    <ViewPost post={post} />
+                    <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+                        <button style={{ backgroundColor: 'var(--red)' }} onClick={() => router.push('/create/')}>Back</button>
+                        <button onClick={confirmPost}>Confirm</button>
+                    </div>
+                </>
+            } />
         </div>
     );
 }
