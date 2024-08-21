7de8f7c2a962d55403bff70250a883c02113ed15
GSDV <sdvgab@gmail.com>
2024-08-21 12:17:59 -0700

    Prod test

--- src/components/pages/post/Confirm.tsx
+++ src/components/pages/post/Confirm.tsx
@@ -33,7 +33,7 @@ export default function ConfirmPost({ post }: { post: Post }) {
 
     return (
         <div style={{ padding: '20px', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '40px' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', gap: '5px' }} >
+            <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center', gap: '5px' }} >
                 <h1>Confirm your post</h1>
                 <h2>You are allowed to edit later</h2>
                 {alert && <Alert alert={alert} variations={[]} />}
