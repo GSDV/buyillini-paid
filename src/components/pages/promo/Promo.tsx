527e0b8fb96042df50599335b3abcdad82859303
GSDV <sdvgab@gmail.com>
2024-08-21 21:37:15 -0700

    promo load

--- src/components/pages/promo/Promo.tsx
+++ src/components/pages/promo/Promo.tsx
@@ -4,6 +4,8 @@ import Form, { FormInputType } from '@components/Form';
 import { Alert, AlertType } from '@components/Alert';
 
 import formStyles from '@styles/ui/form.module.css';
+import { CheckIfLoading } from '@components/Loading';
+import CenterLayout from '@components/containers/CenterLayout';
 
 
 
@@ -13,8 +15,10 @@ export default function Promo() {
     ];
 
     const [alert, setAlert] = useState<AlertType | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const attemptRedeem = async (formData: FormData) => {
+        setLoading(true);
         const res = await fetch('/promo/api/', {
             method: 'POST',
             body: JSON.stringify({promoCode: formData.get('code')}),
@@ -22,8 +26,50 @@ export default function Promo() {
         });
         const resJson = await res.json();
         setAlert(resJson);
+        setLoading(false);
     }
 
+    return (
+        <div className={formStyles.container}>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <h2 className={formStyles.title}>Redeem a promo code</h2>
+                <h4>Get a few free months.</h4>
+            </div>
+            <Form action={attemptRedeem} inputs={inputs} submitTitle='Redeem Code' />
+            {alert && <Alert alert={alert} variations={[]} />}
+        </div>
+    );
+}
+
+function Prkomo() {
+
+    const [alert, setAlert] = useState<AlertType | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const attemptRedeem = async (formData: FormData) => {
+        setLoading(true);
+        const res = await fetch('/promo/api/', {
+            method: 'POST',
+            body: JSON.stringify({promoCode: formData.get('code')}),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const resJson = await res.json();
+        setAlert(resJson);
+        setLoading(false);
+    }
+
+    return (
+        <CenterLayout>
+            <CheckIfLoading loading={loading} content={<PromoForm alert={alert} attemptRedeem={attemptRedeem} />} />
+        </CenterLayout>
+    )
+}
+
+function PromoForm({ alert, attemptRedeem }: { alert: AlertType | null, attemptRedeem: (data: FormData)=>void }) {
+    const inputs: FormInputType[] = [
+        {title: 'Promo Code', type: 'text', name: 'code'}
+    ];
+    
     return (
         <div className={formStyles.container}>
             <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
