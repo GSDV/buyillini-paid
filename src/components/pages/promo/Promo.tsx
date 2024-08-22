6221c5c08580e7205b6522712b81049e5f7084e8
GSDV <sdvgab@gmail.com>
2024-08-21 22:12:11 -0700

    promo load

--- src/components/pages/promo/Promo.tsx
+++ src/components/pages/promo/Promo.tsx
@@ -10,39 +10,6 @@ import CenterLayout from '@components/containers/CenterLayout';
 
 
 export default function Promo() {
-    const inputs: FormInputType[] = [
-        {title: 'Promo Code', type: 'text', name: 'code'}
-    ];
-
-    const [alert, setAlert] = useState<AlertType | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-
-    const attemptRedeem = async (formData: FormData) => {
-        setLoading(true);
-        const res = await fetch('/promo/api/', {
-            method: 'POST',
-            body: JSON.stringify({promoCode: formData.get('code')}),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        const resJson = await res.json();
-        setAlert(resJson);
-        setLoading(false);
-    }
-
-    return (
-        <div className={formStyles.container}>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <h2 className={formStyles.title}>Redeem a promo code</h2>
-                <h4>Get a few free months.</h4>
-            </div>
-            <Form action={attemptRedeem} inputs={inputs} submitTitle='Redeem Code' />
-            {alert && <Alert alert={alert} variations={[]} />}
-        </div>
-    );
-}
-
-function Prkomo() {
-
     const [alert, setAlert] = useState<AlertType | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
 
