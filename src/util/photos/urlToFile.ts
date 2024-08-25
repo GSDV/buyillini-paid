2b113aa4031ea30e104bfde4f611a7231ddf2a76
GSDV <sdvgab@gmail.com>
2024-08-25 17:29:12 -0500

    Prod test

--- src/util/photos/urlToFile.ts
+++ src/util/photos/urlToFile.ts
@@ -1,8 +1,12 @@
 'use client';
 
+import { imgUrl } from "@util/global";
+
+
+
 export async function urlToFile(s3Url: string) {
     console.log(s3Url)
-    const response = await fetch(s3Url);
+    const response = await fetch(imgUrl(s3Url));
     console.log("is ok:", response.ok)
     if (!response.ok) return null;
     const blob = await response.blob();
