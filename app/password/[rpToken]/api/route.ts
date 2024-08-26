0f2a58d530805e0066e6de1b85471c64e8afe15d
GSDV <sdvgab@gmail.com>
2024-08-26 14:02:32 -0500

    Prod test

--- app/password/[rpToken]/api/route.ts
+++ app/password/[rpToken]/api/route.ts
@@ -9,9 +9,10 @@ import { isRPTokenExpired } from '@util/api/tokens';
 
 
 // Reset Password Token and new password have been provided, and now the account's password must be updated
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest, { params }: { params: { rpToken: string } }) {
     try {
-        const { rpToken, newPassword } = await req.json();
+        const { newPassword } = await req.json();
+        const rpToken = params.rpToken
         if (!isValidPassword(newPassword)) return NextResponse.json({ cStatus: 102, msg: `Use a password with 5 to 50 characters, consisting only of letters, numbers, and the symbols #, $, %, and &.` }, { status: 400 });
         
         if (!rpToken) return NextResponse.json({ cStatus: 101, msg: `No reset password token provided.`}, { status: 400 });
@@ -19,7 +20,7 @@ export async function POST(req: NextRequest) {
         if (!rpTokenPrisma) return NextResponse.json({ cStatus: 501, msg: `Did not provide a real reset password token.` }, { status: 400 });
         if (isRPTokenExpired(rpTokenPrisma)) return NextResponse.json({ cStatus: 502, msg: `This reset password token has expired.` }, { status: 400 });
 
-        const userPrisma = await getUser(rpToken.userId);
+        const userPrisma = await getUser(rpTokenPrisma.userId);
         // Since we found a real token, there must be a real user associated with it. So the following should in theory never run, but just in case
         if (!userPrisma) return NextResponse.json({ cStatus: 400, msg: `Unknown server error.`}, { status: 400 });
 
