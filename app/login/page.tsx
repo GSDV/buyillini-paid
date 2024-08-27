c091dc419f09b2df8f09b36a96184db9d725bd49
GSDV <sdvgab@gmail.com>
2024-08-27 12:08:47 -0500

    Prod test

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,6 +15,7 @@ export default function Page() {
     const checkIfAlreadyLoggedIn = async () => {
         const res = await fetch(`/login/api`, { method: 'GET' });
         const resJson = await res.json();
+        console.log("A", resJson)
         if (resJson.cStatus==201) router.push(`/account/${resJson.netId}`);
     }
 
