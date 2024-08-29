60e37ce26bed3efe420e0c8878332a2b91bd965c
GSDV <sdvgab@gmail.com>
2024-08-29 11:44:10 -0500

    Prod test

--- app/admin/posts/super/api/route.ts
+++ app/admin/posts/super/api/route.ts
@@ -9,36 +9,27 @@ import { getRedactedUserFromAuth } from '@util/prisma/actions/user';
 
 export async function POST(req: NextRequest) {
     try {
-        console.log("AAA");
         const authTokenCookie = cookies().get('authtoken');
-        if (!authTokenCookie) return NextResponse.json({ cStatus: 400, msg: `Unauthorized.` }, { status: 400 });
 
-        console.log("BBB");
         const resPermissions = await isAdmin(authTokenCookie);
         if (!resPermissions) return NextResponse.json({ cStatus: 400, msg: `Unauthorized.` }, { status: 400 });
 
-        console.log("CCC");
         // For TypeScript:
-        const adminPrisma = await getRedactedUserFromAuth(authTokenCookie.value);
+        const adminPrisma = await getRedactedUserFromAuth((authTokenCookie as any).value);
         if (!adminPrisma)return NextResponse.json({ cStatus: 400, msg: `Unauthorized.` }, { status: 400 });
 
-        console.log("DDD");
         const { inputData } = await req.json();
         if (!inputData) return NextResponse.json({ cStatus: 101, msg: `No inputData provided.` }, { status: 400 });
 
-        console.log("EEE", inputData);
         // This is a synchronous operation, but deployment with Vercel somehow makes it return a promise.
         // Hence the unnecessary "await"
-        const resValidPost = isValidInputSuperPostData(inputData);
-        console.log("FFF resValidPost:", resValidPost)
+        const resValidPost = await isValidInputSuperPostData(inputData);
         if (!resValidPost.valid) return NextResponse.json({ cStatus: 102, TESTING: "HELLO", msg: resValidPost.msg }, { status: 400 });
 
         // Also a synchronous process, same problem as above.
-        const postData = superPostDataFromInputs(inputData);
-        console.log("GGG postData:", postData);
+        const postData = await superPostDataFromInputs(inputData);
 
         const postId = await createSuperPost(postData, adminPrisma.id);
-        console.log("HHH postId:", postId)
         
         return NextResponse.json({ cStatus: 200, msg: `Success.`, postId: postId }, { status: 200 });
     } catch(err) {
