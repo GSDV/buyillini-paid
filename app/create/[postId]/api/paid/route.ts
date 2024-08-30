743e7686b6224cb7bcb65a34479e44ed2ef9c51c
GSDV <sdvgab@gmail.com>
2024-08-30 13:47:12 -0500

    Prod test

--- app/create/[postId]/api/paid/route.ts
+++ app/create/[postId]/api/paid/route.ts
@@ -81,6 +81,7 @@ export async function GET(req: NextRequest, { params }: { params: { postId: stri
 // After confirming paid post, mark it active and redirect to newly created post.
 export async function PUT(req: NextRequest, { params }: { params: { postId: string } }) {
     try {
+        console.log("AAAA")
         const postId = params.postId;
 
         if (!postId) return NextResponse.json({ cStatus: 101, msg: `No postId provided.` }, { status: 400 });
@@ -100,8 +101,11 @@ export async function PUT(req: NextRequest, { params }: { params: { postId: stri
         if (!postPrisma.isPaid) return NextResponse.json({ cStatus: 431, msg: `This post is a free post.` }, { status: 400 });
         if (userPrisma.freeMonths < postPrisma.freeMonthsUsed) return NextResponse.json({ cStatus: 102, msg: `Not enough free months.` }, { status: 400 });
 
+        console.log("AAA")
         
         const months = postPrisma.duration - postPrisma.freeMonthsUsed;
+
+        console.log("BBB")
 
         const session = await stripe.checkout.sessions.create({
             line_items: [ { price: process.env.STRIPE_BUYILLINI_ONE_MONTH_PRICE_ID, quantity: months } ],
@@ -110,9 +114,11 @@ export async function PUT(req: NextRequest, { params }: { params: { postId: stri
             cancel_url: `${DOMAIN}/create/${postPrisma.id}/paid/stripe/error`,
             automatic_tax: { enabled: true }
         });
+        console.log("CCC", session)
         
         await deleteAllFailedPostPayments(postPrisma.id);
         await addPaymentToPost(postPrisma.id, session.id, months);
+        console.log("DDD")
 
         return NextResponse.json({ cStatus: 200, msg: `Success.`, postId: postId }, { status: 200 });
     } catch(err) {
